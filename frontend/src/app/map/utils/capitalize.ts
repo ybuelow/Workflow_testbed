acc8c46b7cae67ae4c4bb632c64c08ccdda015a5
ybuelow <ybuelow@users.noreply.github.com>
2025-01-08 10:50:07 +0000

    deploy: d8a951812bcb6eee184715b580a7d34c3aa1636d

--- frontend/src/app/map/utils/capitalize.ts
+++
@@ -1,6 +0,0 @@
-/**
- *funcktion to capitalize the first letter of a String
- */
-export function capitalizeFirstLetter(string: string): string {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
